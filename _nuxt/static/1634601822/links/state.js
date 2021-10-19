9fe499d34fac7341e7cfab3804c7db0047cd8cdf
wowkster <wowkster@users.noreply.github.com>
2021-10-19 00:07:19 +0000

    deploy: 9c2fbd5f609570cc92a09120b63658bcd525899a

--- _nuxt/static/1634601822/links/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634601822",layout:"default",error:a,serverRendered:true,routePath:"\u002Flinks",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1749210b"}}}}(null));
